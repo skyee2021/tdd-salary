41cdc0bcb1697dddf8f6fdb6b547bc88b31fc551
Skye <skyee20211@gmail.com>
2021-04-11 01:52:53 +0800

    only monthly tdd-test

--- lib/money.rb
+++ lib/money.rb
@@ -1,52 +1,56 @@
-require 'user'
+require 'employ'
 
-class Bike
+class Money
   
-  def initialize(fee, user: nil)
-    @fee = fee
-    @user = user
-    @begin = nil
-    @end = nil
+  def initialize(income, employ: nil)
+    @pay = pay
+    @employ = employ
+    @income = income
   end
 
-  def rent(time)
-    @begin = time
-  end
+  # def calc_to_hr(income)
+  #   # @income = income
+  #   # p income
+  #   @calc_to_hr = (income / 30 / 8 )
+  #   p calc_to_hr
+  # end
 
-  def back(time)
-    @end = time
-  end
-
-  def times(mins)
-    @fee = (mins/30) * 10 if mins <=240
-    #~4hrs
-    @fee = (mins-240)/30 * 20 + 80 if mins >240 and  mins <= 480
-    #4hrs~8hrs
-    @fee = (mins-480)/30  * 40 + 80 + 160 if mins > 480
-  end
-
-  def time(mins)
-    @fee = 5 if mins <= 30
-    #不滿30mins
-    # p @fee
-    @fee = (mins/30) * 10 if mins >30 and mins <= 240
-    # p @fee
-    #30mins~4hrs
-    @fee = (mins-240)/30 * 20 + 80 if mins >240 and  mins <= 480
-    # p @fee
-    #4hrs~8hrs
-    @fee = (mins-480)/30  * 40 + 80 + 160 if mins > 480
-    # p @fee
-  end
-
-  def fee(mins = nil)
-    mins = ((@end - @begin) / 60).to_i
-    if @user.login?
-      time(mins)
+  def add_w(hrs)
+    #半小時計算
+    
+    calc_to_hr = (@income / 30 / 8 ).to_i
+    p calc_to_hr
+    case hrs
+    when 0..2
+      @pay = (calc_to_hr * hrs * 1.34).to_i
+    when 2.5..8
+      @pay = ((calc_to_hr * 2 * 1.34) + calc_to_hr * (hrs - 2) * 1.67).to_i
     else
-      times(mins)
+      @pay = ((calc_to_hr * 2 * 1.34) + (calc_to_hr * 6 * 1.67) + calc_to_hr * (hrs - 8) * 1.67).to_i
     end
-    @fee
+  end
+
+  def add_h(hrs)
+    calc_to_hr = (@income / 30 / 8 ).to_i
+    p calc_to_hr
+    # p calc_to_hr * 8
+    case hrs
+    when 0..8
+      @pay = calc_to_hr * 8
+      # puts "0..8_#{pay}"
+    when 8.5..10
+      @pay = (calc_to_hr * 8 + (calc_to_hr * (hrs-8) * 2.34)).to_i
+      # puts "8.5..10_#{pay}"
+    else
+      @pay = (calc_to_hr * 8 + (calc_to_hr * (2) * 2.34) + calc_to_hr * (hrs-10) * 2.67).to_i
+      # puts "10up_#{pay}"
+    end     
+  end
+
+  def pay
+    
+    @pay
+    
   end
 
 
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -3,93 +3,39 @@ require 'employ'
 
 
 RSpec.describe Money do
-  context "平日與休息日" do
-    let(:user) { User.new(true) }) }
-    it "in 4hrs" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      bike.rent(now)
-      bike.back(now + 60 * 60 * 3)
-      # bike.times(180)
-      expect(bike.fee).to be 60
-      #n*2*10
+  context "月薪加班費" do
+    let(:employ) { Employ.new(true) }
+    it "平日或休息日" do
+      money = Money.new(30000, employ: employ)
+      # money.calc_to_hr(30000)
+      money.add_w(4)
+      expect(money.pay).to be 752
     end
 
-    it "4hrs - 8hrs" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      bike.rent(now)
-      bike.back(now + 60 * 60 * 6)
-      # bike.times(360)
-      expect(bike.fee).to be 160
-      #4*2*10+(n-4)*2*20
+    it "平日或休息日" do
+      money = Money.new(45000, employ: employ)
+      # money.calc_to_hr(30000)
+      money.add_w(7.5)
+      expect(money.pay).to be 2218
     end
 
-    it "over 8hrs" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      bike.rent(now)
-      bike.back(now + 60 * 60 * 9)
-      # bike.times(540)
-      expect(bike.fee).to be 320
-      #80+160+(n-8)*2*40
-    end
-  end
-
-  context '假日' do
-    let(:user) { User.new(true) }
-    it "in 30mins" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      Timecop.freeze(now)
-      bike.rent(Time.now)
-      Timecop.travel(now + 60 * 60 * 0.5)
-      bike.back(Time.now)
-      # bike.time(30)
-      expect(bike.fee).to be 5
-    end
-  
-    it "30mins 到 4hrs" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      Timecop.freeze(now)
-      bike.rent(Time.now)
-      Timecop.travel(now + 60 * 60 * 2)
-      bike.back(Time.now)
-      # now = Time.now
-      # bike.rent(now)
-      # bike.back(now + 60 * 60 * 2)
-      expect(bike.fee).to be 40
-      #2hrs=>4*10
+    it "假日" do
+      money = Money.new(30000, employ: employ)
+      money.add_h(5)
+      expect(money.pay).to be 1000
     end
 
-    it "4hrs - 8hrs" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      Timecop.freeze(now)
-      bike.rent(Time.now)
-      Timecop.travel(now + 60 * 60 * 5)
-      bike.back(Time.now)      
-      expect(bike.fee).to be 120
-      #4*2*10+2*20
+    it "假日" do
+      money = Money.new(50000, employ: employ)
+      money.add_h(8.5)
+      expect(money.pay).to be 1907
     end
 
-    it "8hrs up" do
-      bike = Bike.new(0, user: user)
-      now = Time.now
-      Timecop.freeze(now)
-      bike.rent(Time.now)
-      Timecop.travel(now + 60 * 60 * 9)
-      bike.back(Time.now)
-      # bike.time(540)
-      expect(bike.fee).to be 320
-      #4*2*10+4*2*20+2*20
+    it "假日" do
+      money = Money.new(50000, employ: employ)
+      money.add_h(12)
+      expect(money.pay).to be 3748
     end
-  
-
-  
 
   end
-
-
 end
